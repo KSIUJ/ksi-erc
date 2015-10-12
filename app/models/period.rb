040390c05dccd8bfb45d4d865d360c098c1797ec
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-10-13 00:32:44 +0200

    Period should not be unique.

--- app/models/period.rb
+++ app/models/period.rb
@@ -13,6 +13,6 @@
 class Period < ActiveRecord::Base
   validates :fee, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0}, presence: true
   validates :info, presence: true, length: { maximum: 30}
-  validates :academic_year, presence: true, uniqueness: true, format: { with: /\A\d\d\d\d\/\d\d\d\d\z/ }
+  validates :academic_year, presence: true, format: { with: /\A\d\d\d\d\/\d\d\d\d\z/ }
   has_many :members, through: :memberships
 end
