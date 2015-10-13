ee915f2fa0a27ba30fda54f015cdd10e9a4edf0a
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-10-13 23:08:45 +0200

    Basic styling.

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'haml-rails', '~> 0.9'
 gem 'sorcery', '~>0.9.1'
 gem 'pg', '~> 0.18.3'
 gem 'validates_email_format_of', '~> 1.6.3'
+gem 'bootstrap-sass', '~> 3.3.5'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+  // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -14,3 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require bootstrap-sprockets
