078d5a26ca139089625ce9c2ae6877307442360b
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-11-02 02:25:53 +0100

    Updated run script.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 dropdb -U ksi_erc ksi_erc_development
 createdb -U ksi_erc ksi_erc_development
 rake db:migrate
-psql -U ksi_erc ksi_erc_development < ksi_erc.sql
+#psql -U ksi_erc -d ksi_erc_development -f ksi_erc.sql
+pg_restore -U ksi_erc -d ksi_erc_development --data-only ksi_erc.tar
