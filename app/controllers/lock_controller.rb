de5ff54406bebdee643e84b2d0b25304174acadf
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-11-02 01:19:52 +0100

    Implemented check for paid membership in lock authorization.

--- app/controllers/lock_controller.rb
+++ app/controllers/lock_controller.rb
@@ -5,14 +5,23 @@ class LockController < ApplicationController
   def authorize
     card_id = params[:card_id]
     if @@CARD_ID_REGEX.match(card_id)
-      File.open('tmp/last_card_id.txt', 'w') do |f|
-        f.puts card_id
-      end
-      @member = Member.where("card_id = ?", card_id)
-      if @member.any?
+      @member = Member.find_by card_id: card_id
+      if !@member.nil? && has_current(@member.memberships)
         return head :ok
       end
     end
     return head :forbidden
+  end
+
+  private
+  def has_current(memberships)
+    now = Time.now.to_date
+    pivot = Time.new(now.year, 10, 31).to_date
+    current_period = if now < pivot
+      "#{pivot.year - 1}/#{pivot.year}"
+    else
+      "#{pivot.year}/#{pivot.year + 1}"
+    end
+    memberships.joins(:period).exists?(periods: { academic_year: current_period }, fee_paid: true)
   end
 end
