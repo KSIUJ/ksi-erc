5e67d02f908055e815eb9499b361f655b8c51fb5
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2018-07-30 12:40:47 +0200

    Implement tests for Member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -12,21 +12,20 @@
 #
 
 class Member < ApplicationRecord
-  validates :name, presence: true
-  validates :surname, presence: true
+  validates :name, :surname, presence: true
   validates :email, presence: true, uniqueness: true,
             email_format: { message: "It's not a valid email."}
   validates :card_id, uniqueness: true, allow_blank: true,
             length: { is: 8, message: "ELS should be 8 characters long." }
 
   has_many :memberships
-  accepts_nested_attributes_for :memberships
+  accepts_nested_attributes_for :memberships # TODO test
   
   has_many :roles, through: :memberships
   has_many :periods, through: :memberships
-  has_one :comment
 
   def full_name
       name + ' ' + surname
   end
 end
+
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -14,7 +14,66 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @member = members(:valid)
+  end
+
+  test 'valid' do
+    assert @member.valid?
+  end
+
+  test 'invalid without name' do
+    @member.name = nil
+    refute @member.valid?
+    assert_not_nil @member.errors[:name]
+  end
+
+  test 'invalid without surname' do
+    @member.surname = nil
+    refute @member.valid?
+    assert_not_nil @member.errors[:surname]
+  end
+
+  test 'invalid without email' do
+    @member.email = nil
+    refute @member.valid?
+    assert_not_nil @member.errors[:email]
+  end
+  
+  test 'invalid with same card id' do
+    duplicate = Member.new(name: 'a', surname: 'a', email: 'a@a.com', card_id: 'aabb1122')
+    refute duplicate.valid?
+    assert_not_nil duplicate.errors[:card_id]
+  end
+
+  test 'invalid with length of card id different than 8' do
+    @member.card_id = 'a'
+    refute @member.valid?
+    assert_not_nil @member.errors[:card_id]
+    @member.card_id = 'aaaaaaaa11'
+    refute @member.valid?
+    assert_not_nil @member.errors[:card_id]
+  end
+
+  test 'valid with blank card_id' do
+    @member.card_id = ''
+    assert @member.valid?
+  end
+
+  test '#memberships' do
+    assert_equal 2, @member.memberships.size
+  end
+
+  test '#roles' do
+    assert_equal 2, @member.roles.size
+  end
+
+  test '#periods' do
+    assert_equal 2, @member.periods.size
+  end
+  
+  test '#full_name' do
+    assert_equal 'John Doe', @member.full_name
+  end
 end
+
