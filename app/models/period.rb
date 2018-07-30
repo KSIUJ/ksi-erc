2b62755b2b54f69f726d497bd1a8f315aa6de298
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2018-07-30 16:48:56 +0200

    Implement Period model tests

--- app/models/period.rb
+++ app/models/period.rb
@@ -11,12 +11,25 @@
 #
 
 class Period < ApplicationRecord
-  validates :fee, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than_or_equal_to: 0}, presence: true
+  ACADEMIC_YEAR_REGEX = /\A(\d{4})\/(\d{4})\z/ 
+  validates :fee, numericality: {greater_than_or_equal_to: 0}
   validates :info, presence: true, length: { maximum: 30}
-  validates :academic_year, presence: true, format: { with: /\A\d\d\d\d\/\d\d\d\d\z/ }
+  validates :academic_year, presence: true
+  validates_each :academic_year do |record, attr, value|
+    if value && match = value.match(ACADEMIC_YEAR_REGEX)
+      old, new = match.captures.map{ |s| s.to_i }
+      if old + 1 != new
+        record.errors.add(attr, 'years must be consecutive')
+      end
+    else
+      record.errors.add(attr, 'must be in format xxxx/xxxx')
+    end
+  end
+      
   has_many :memberships
   has_many :members, through: :memberships
 
+  # TODO check if this is even used if yes then write test
   def concat_year_and_info
     self.academic_year + ' ' + self.info
   end
--- test/models/test_period.rb
+++ test/models/test_period.rb
@@ -27,9 +27,54 @@ class PeriodTest < ActiveSupport::TestCase
     assert_not_nil @period.errors[:fee]
   end
 
-  test 'invalid when fee is in wrong format' do
-    @period.fee = '.00'
+  test 'invalid when fee is not a number' do
+    @period.fee = 'a'
     refute @period.valid?
+    assert_not_nil @period.errors[:fee]
+  end
+
+  test 'invalid without info' do
+    @period.info = nil
+    refute @period.valid?
+    assert_not_nil @period.errors[:info]
+  end
+
+  test 'invalid with too long info' do
+    @period.info = 'a' * 31
+    refute @period.valid?
+    assert_not_nil @period.errors[:info]
+  end
+
+  test 'invalid without academic year' do
+    @period.academic_year = nil
+    refute @period.valid?
+    assert_not_nil @period.errors[:info]
+  end
+
+  test 'invalid when academic year is in wrong format' do
+    @period.academic_year = '0/0'
+    refute @period.valid?
+    assert_not_nil @period.errors[:info]
+    @period.academic_year = '2016'
+    refute @period.valid?
+    assert_not_nil @period.errors[:info]
+    @period.academic_year = '2016/20'
+    refute @period.valid?
+    assert_not_nil @period.errors[:info]
+    @period.academic_year = '16/17'
+    refute @period.valid?
+    assert_not_nil @period.errors[:info]
+    @period.academic_year = '2016/2018'
+    refute @period.valid?
+    assert_not_nil @period.errors[:info]
+  end
+
+  test '#memberships' do
+    assert_equal 1, @period.memberships.size
+  end
+
+  test '#members' do
+    assert_equal 1, @period.members.size
   end
 end
 
