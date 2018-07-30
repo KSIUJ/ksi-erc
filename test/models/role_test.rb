59139aae9ae210a0f9ed06a52ec35357f2361c66
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2018-07-30 15:41:36 +0200

    Implement Role model tests

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -11,5 +11,36 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
+  def setup
+    @role = roles(:common)
+  end
+  
+  test 'valid' do
+    assert @role.valid?
+  end
+  
+  test 'invalid without name' do
+    @role.name = nil
+    refute @role.valid?
+    assert_not_nil @role.errors[:name]
+  end
+
+  test 'invalid with duplicate name' do
+    duplicate = Role.new(name: 'Common')
+    refute duplicate.valid?
+    assert_not_nil duplicate.errors[:name]
+  end
+
+  test '#memberships' do
+    assert_equal 1, @role.memberships.size
+  end
+
+  test '#members' do
+    assert_equal 1, @role.members.size
+  end
+
+  test '#to_s' do
+    assert_equal @role.to_s, 'Common'
+  end
 end
 
