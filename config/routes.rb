74501dc2bb5e5a09ec81db0877fb42ee13cfe9ba
Adam Piekarczyk <adam.jan.piekarczyk@gmail.com>
2019-01-08 12:11:09 +0100

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
   post '/lock/authorize', to: 'lock#authorize'
-  get '/authorize', to 'lock#legacy_authorize'
+  get '/authorize', to: 'lock#legacy_authorize'
 
   post '/memberships/prolong', to: 'memberships#prolong'
   resources :memberships
