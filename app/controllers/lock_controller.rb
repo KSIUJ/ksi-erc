a36141ce1ce4e6f9399d9dee8b1f8d1c9059c09c
Adam Piekarczyk <adam.jan.piekarczyk@gmail.com>
2019-01-08 12:09:49 +0100

    Hotfix require login

--- app/controllers/lock_controller.rb
+++ app/controllers/lock_controller.rb
@@ -1,30 +1,25 @@
 class LockController < ApplicationController
-  skip_before_action :require_login
+  skip_before_action :authenticate_user!, only: [:authorize]
   @@CARD_ID_REGEX = /^[0-9a-f]{8}$/
 
   def authorize
+    card_id = params[:card_id]
+    lock_name = params[:lock_name]
+    puts Rails.configuration.locks.known[lock_name]  
+  end
+
+  # todo delete this when ksi-lock code is finally fixed
+  def legacy_authorize
     card_id = params[:card_id]
     if @@CARD_ID_REGEX.match(card_id)
       File.open('tmp/last_card_id.txt', 'w') do |f|
         f.puts card_id
       end
       @member = Member.find_by card_id: card_id
-      if !@member.nil? && has_current(@member.memberships)
+      if !@member.nil?
         return head :ok
       end
     end
     return head :forbidden
-  end
-
-  private
-  def has_current(memberships)
-    now = Time.now.to_date
-    pivot = Time.new(now.year, 10, 31).to_date
-    current_period = if now < pivot
-      "#{pivot.year - 1}/#{pivot.year}"
-    else
-      "#{pivot.year}/#{pivot.year + 1}"
-    end
-    memberships.joins(:period).exists?(periods: { academic_year: current_period }, fee_paid: true)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
-  get 'lock/authorize'
+  post '/lock/authorize', to: 'lock#authorize'
+  get '/authorize', to 'lock#legacy_authorize'
 
   post '/memberships/prolong', to: 'memberships#prolong'
   resources :memberships
   resources :periods
   resources :roles
   resources :members
-  #resources :sessions, only: [:new, :create, :destroy]
-  #get '/login', to: 'sessions#new', as: :login
-  #delete '/logout', to: 'sessions#destroy', as: :logout
-  #resources :users
-  #get '/signup', to: 'users#new', as: :signup
-  get '/authorize', to: 'lock#authorize', as: :authorize
   root 'pages#index'
 end
