7237a67a6faf929b6607c924b778a57e5cf85424
Adam Piekarczyk <adam.jan.piekarczyk@gmail.com>
2019-01-09 09:56:10 +0100

    Remove unnecessary parameters

--- app/controllers/lock_controller.rb
+++ app/controllers/lock_controller.rb
@@ -1,5 +1,5 @@
 class LockController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:authorize, :legacy_authorize], raise: false
+  skip_before_action :authenticate_user!
   @@CARD_ID_REGEX = /^[0-9a-f]{8}$/
 
   def authorize
